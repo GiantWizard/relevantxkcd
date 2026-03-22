bf5f328056d872a6175ff99c0aa1a17f546fced9
GiantWizard <bobofrogoo@gmail.com>
2026-03-22 15:37:47 -0500

    fixed dockerfile and github sync

--- update.sh
+++ update.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
-# Wrapper script to run updates inside the current virtual environment 
-
-# Get to the exact script directory
 cd "$(dirname "$0")"
 
-# Execute our python spider
-# This correctly appends any new, unindexed comics to explanations.txt
 python xkcd.py
 
-# Next we run a quick internal mode in main.py to just update vectors.txt without starting the cli
 python -c "
 import main
 from sentence_transformers import SentenceTransformer
@@ -17,4 +10,4 @@ comics = main.loadComics()
 # Build vectors saves the updated dict out automatically:
 main.buildVectors(comics, embedModel)
 "
-echo "All systems synced and updated!"
+echo "all systems synced and updated"
